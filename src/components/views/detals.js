4704df116d4a72647052b9a7405ee06dfcdacd38
AlekseySknar <59883118+AlekseySknar@users.noreply.github.com>
2020-02-13 11:36:08 +0700

    Form

--- src/components/views/detals.js
+++ src/components/views/detals.js
@@ -6,12 +6,33 @@ import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import CustomizedSteppers from "./steps";
+import TextField from "@material-ui/core/TextField";
 
 class Detals extends React.Component {
   render() {
     return (
       <div>
+        <Typography variant="h3" style={{ textAlign: "left" }}>
+          Заявка.
+        </Typography>
         <CustomizedSteppers />
+        <Typography variant="h3" style={{ textAlign: "left" }}>
+          Тип устройства:
+        </Typography>
+        <Button variant="outlined">Смартфон</Button>
+        <Button variant="contained" color="primary">
+          Ноутбук
+        </Button>
+        <Button variant="outlined">Планшет</Button>
+        <Typography variant="h3" style={{ textAlign: "left" }}>
+          Выберете марку:
+        </Typography>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          fullWidth
+        />
       </div>
     );
   }
